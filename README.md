cb0ecb0c2e419722aee92791e6abaddc1b2d556a
v-oxel <v.oxel@mail.ru>
2022-06-13 23:30:22 +0300

    readme edited. some comments for xorcrpt.c

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # xorcrpt
 XOR Cryptor
+
+# Compile
+```sh
+gcc xorcrpt.c -o xorcrpt -O3 -Wall
+```
+
+# Usage
+```sh
+./xorcrpt <filename> <key>
+```
--- xorcrpt.c
+++ xorcrpt.c
@@ -1,76 +1,90 @@
-#include <locale.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
+
+/*
+ * load file to buffer
+ *          ||
+ *          \/
+ *     xor encrypt
+ *          ||
+ *          \/
+ * write buffer to file
+ */
 
 int main(int argc, const char* argv[])
 {
-	if (argc != 3)
-		return 1;
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: ./xorcrpt <filename> <key>\n");
+        return 1;
+    }
 
-	setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "");
 
-	FILE* srcf;       /* source file */
-	char* buf;        /* buffer for file */
-	size_t buf_size;  /* size of file */
-	const char* key;  /* (en/de)cryption key */
-	size_t i;
+    FILE* srcf;       /* source file */
+    char* buf;        /* buffer for file */
+    size_t buf_size;  /* size of file */
+    const char* key;  /* (en/de)cryption key */
+    size_t i;
 
-	srcf = fopen(argv[1], "rb");
-	if (!srcf)
-	{
-		fprintf(stderr, "srcf rb is NULL\n");
-		return 1;
-	}
+    srcf = fopen(argv[1], "rb");
+    if (!srcf)
+    {
+        fprintf(stderr, "srcf rb is NULL\n");
+        return 1;
+    }
 
-	fseek(srcf, 0, SEEK_END);
-	buf_size = ftell(srcf);
-	fseek(srcf, 0, SEEK_SET);
-	buf = (char*)malloc(buf_size);
-	if (!buf)
-	{
-		fprintf(stderr, "buf is NULL\n");
-		fclose(srcf);
-		return 1;
-	}
-	if (fread(buf, 1, buf_size, srcf) != buf_size)
-	{
-		fprintf(stderr, "fread != buf_size\n");
-		fclose(srcf);
-		free(buf);
-		return 1;
-	}
-	fclose(srcf);
+    /* loading file to buffer */
+    fseek(srcf, 0, SEEK_END);
+    buf_size = ftell(srcf);
+    fseek(srcf, 0, SEEK_SET);
+    buf = (char*)malloc(buf_size);
+    if (!buf)
+    {
+        fprintf(stderr, "buf is NULL\n");
+        fclose(srcf);
+        return 1;
+    }
+    if (fread(buf, 1, buf_size, srcf) != buf_size)
+    {
+        fprintf(stderr, "fread != buf_size\n");
+        fclose(srcf);
+        free(buf);
+        return 1;
+    }
+    fclose(srcf);
 
-	key = argv[2];
-
-
-	for (i = 0; i < buf_size; ++i)
-	{
-		buf[i] ^= *key++;
-		if (!*key)
-			key = argv[2];
-	}
+    key = argv[2];
 
 
+    /* xor encryption alg */
+    for (i = 0; i < buf_size; ++i)
+    {
+        buf[i] ^= *key++;
+        if (!*key)
+            key = argv[2];
+    }
 
-	srcf = fopen(argv[1], "wb");
-	if (!srcf)
-	{
-		fprintf(stderr, "srcf wb is NULL\n");
-		free(buf);
-		return 1;
-	}
+    /* writing buffer to file */
+    srcf = fopen(argv[1], "wb");
+    if (!srcf)
+    {
+        fprintf(stderr, "srcf wb is NULL\n");
+        free(buf);
+        return 1;
+    }
 
-	if (fwrite(buf, 1, buf_size, srcf) != buf_size)
-	{
-		fprintf(stderr, "fwrite != buf_size\n");
-		fclose(srcf);
-		free(buf);
-		return 1;
-	}
-	fclose(srcf);
-	free(buf);
+    if (fwrite(buf, 1, buf_size, srcf) != buf_size)
+    {
+        fprintf(stderr, "fwrite != buf_size\n");
+        fclose(srcf);
+        free(buf);
+        return 1;
+    }
+    fclose(srcf);
+    free(buf);
 
-	return 0;
+    return 0;
 }
