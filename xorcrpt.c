dc5cb3d3de0f63653fb474f3b4ea654fc9c9c381
v-oxel <v.oxel@mail.ru>
2022-06-13 23:07:35 +0300

    xorcrpt.c created

---
+++ xorcrpt.c
@@ -0,0 +1,76 @@
+#include <locale.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char* argv[])
+{
+	if (argc != 3)
+		return 1;
+
+	setlocale(LC_ALL, "");
+
+	FILE* srcf;       /* source file */
+	char* buf;        /* buffer for file */
+	size_t buf_size;  /* size of file */
+	const char* key;  /* (en/de)cryption key */
+	size_t i;
+
+	srcf = fopen(argv[1], "rb");
+	if (!srcf)
+	{
+		fprintf(stderr, "srcf rb is NULL\n");
+		return 1;
+	}
+
+	fseek(srcf, 0, SEEK_END);
+	buf_size = ftell(srcf);
+	fseek(srcf, 0, SEEK_SET);
+	buf = (char*)malloc(buf_size);
+	if (!buf)
+	{
+		fprintf(stderr, "buf is NULL\n");
+		fclose(srcf);
+		return 1;
+	}
+	if (fread(buf, 1, buf_size, srcf) != buf_size)
+	{
+		fprintf(stderr, "fread != buf_size\n");
+		fclose(srcf);
+		free(buf);
+		return 1;
+	}
+	fclose(srcf);
+
+	key = argv[2];
+
+
+	for (i = 0; i < buf_size; ++i)
+	{
+		buf[i] ^= *key++;
+		if (!*key)
+			key = argv[2];
+	}
+
+
+
+	srcf = fopen(argv[1], "wb");
+	if (!srcf)
+	{
+		fprintf(stderr, "srcf wb is NULL\n");
+		free(buf);
+		return 1;
+	}
+
+	if (fwrite(buf, 1, buf_size, srcf) != buf_size)
+	{
+		fprintf(stderr, "fwrite != buf_size\n");
+		fclose(srcf);
+		free(buf);
+		return 1;
+	}
+	fclose(srcf);
+	free(buf);
+
+	return 0;
+}
